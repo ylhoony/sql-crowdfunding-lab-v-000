302b48ddd9dbc89d04badd1afcc8a0b8e5991df9
Hoon Lee <yl.hoony@gmail.com>
2017-12-22 22:05:49 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 FROM users
 INNER JOIN pledges
 ON users.id = pledges.user_id
-GROUP BY projects.title;"
+GROUP BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
