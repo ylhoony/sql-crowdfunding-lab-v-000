2338f4010d1eb5608a32412bde4002ba2c94a056
Hoon Lee <yl.hoony@gmail.com>
2017-12-22 21:32:44 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO users (name, age) VALUES ("Hash Brown", 22);
+INSERT INTO users (name, age) VALUES ("Natalie Bread", 32);
+INSERT INTO users (name, age) VALUES ("Victoria Secret", 18);
+INSERT INTO users (name, age) VALUES ("Brad Pitt", 42);
+INSERT INTO users (name, age) VALUES ("Pororo Krong", 6);
+INSERT INTO users (name, age) VALUES ("Maria Rock", 23);
+INSERT INTO users (name, age) VALUES ("Matt Cruypenk", 24);
+INSERT INTO users (name, age) VALUES ("Jack	Young", 30);
+INSERT INTO users (name, age) VALUES ("Garry	Hoffman", 28);
+INSERT INTO users (name, age) VALUES ("Jeff	Nichols", 36);
+INSERT INTO users (name, age) VALUES ("Armando	Moreno", 31);
+INSERT INTO users (name, age) VALUES ("Elias	Foster", 49);
+INSERT INTO users (name, age) VALUES ("Luke	Burton", 51);
+INSERT INTO users (name, age) VALUES ("Joshua	Rice", 45);
+INSERT INTO users (name, age) VALUES ("Salvador	Harmon", 34);
+INSERT INTO users (name, age) VALUES ("Allen	Williamson", 23);
+INSERT INTO users (name, age) VALUES ("Alyssa	Cannon", 28);
+INSERT INTO users (name, age) VALUES ("Sadie	Fox", 37);
+INSERT INTO users (name, age) VALUES ("Jean	Brock", 33);
+INSERT INTO users (name, age) VALUES ("Ervin	Garza", 67);
+
+
+
+
