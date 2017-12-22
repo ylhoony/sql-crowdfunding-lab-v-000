ad11af7bb659d5dda2d9a308c421b85986a50b73
Hoon Lee <yl.hoony@gmail.com>
2017-12-22 22:32:18 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -49,7 +49,7 @@ WHERE projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"SELECT projects.category SUM(pledges.amount)
+"SELECT projects.category, SUM(pledges.amount)
 FROM projects
 INNER JOIN pledges
 ON projects.id = pledges.project_id
