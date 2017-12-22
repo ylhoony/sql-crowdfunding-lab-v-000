27aebdd89c0b17a4cd30af04756f38ad486ab349
Hoon Lee <yl.hoony@gmail.com>
2017-12-22 22:29:36 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -43,7 +43,7 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "SELECT projects.category, pledges.amount
 FROM projects
-INNER JOIN pledges
+FULL OUTER JOIN pledges
 WHERE projects.category = 'music';"
 end
 
