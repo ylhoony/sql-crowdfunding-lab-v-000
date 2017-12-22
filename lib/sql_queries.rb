0d932c52b7c3f666f0355a25709edc2d806d4e7d
Hoon Lee <yl.hoony@gmail.com>
2017-12-22 22:10:42 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,13 @@ GROUP BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT projects.title, projects.funding_goal
+FROM projects
+INNER JOIN pledges
+ON projects.id = pledges.project_id
+HAVING SUM(pledges.amount) > projects.funding_goal
+GROUP BY projects.title;
+"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
