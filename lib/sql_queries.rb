f788dbc5a48b779d7f1454eeba5e071dcbd10297
Hoon Lee <yl.hoony@gmail.com>
2017-12-22 22:20:50 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,7 +32,11 @@ HAVING amount_over_goal >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT users.name, SUM(pledges.amount)
+FROM users
+INNER JOIN pledges
+ON users.id = pledges.user_id
+ORDER BY pledges.amount, users.name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
