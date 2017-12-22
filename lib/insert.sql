e9fe3f39aa188b5e5ae4922fd909804267f68042
Hoon Lee <yl.hoony@gmail.com>
2017-12-22 21:49:58 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -21,20 +21,20 @@ INSERT INTO users (name, age) VALUES ("Ervin	Garza", 67);
 
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("Just Another Lamp - More than a lamp", "Design & Tech", 15000, "2017-12-03", "2018-01-11")
+VALUES ("Just Another Lamp - More than a lamp", "Design & Tech", 15000, "2017-12-03", "2018-01-11");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("Seat Wars - Claim your seat on this packed bus!", "Games", 10000, "2017-12-15", "2018-01-29")
+VALUES ("Seat Wars - Claim your seat on this packed bus!", "Games", 10000, "2017-12-15", "2018-01-29");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("Hell Cross", "Comics & Illustration", 2000, "2017-10-05", "2018-02-14")
+VALUES ("Hell Cross", "Comics & Illustration", 2000, "2017-10-05", "2018-02-14");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("Bessie North House Farm & Restaurant", "Food & Craft", 12500, "2017-12-07", "2018-01-06")
+VALUES ("Bessie North House Farm & Restaurant", "Food & Craft", 12500, "2017-12-07", "2018-01-06");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("CNH | Ceramic "Nest" heaters", "Design & Tech", 17000, "2017-12-07", "2018-01-07")
+VALUES ("CNH | Ceramic "Nest" heaters", "Design & Tech", 17000, "2017-12-07", "2018-01-07");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("Rain Dog Farm", "Food & Craft", 28000, "2017-12-02", "2018-01-11")
+VALUES ("Rain Dog Farm", "Food & Craft", 28000, "2017-12-02", "2018-01-11");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("PITTA - Transformative Autonomous 4K Selfie Drone", "Design & Tech", 50000, "2017-10-20", "2018-01-03")
+VALUES ("PITTA - Transformative Autonomous 4K Selfie Drone", "Design & Tech", 50000, "2017-10-20", "2018-01-03");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("Tales of Baneheath - Red", "Comics & Illustration", 25000, "2017-12-01", "2018-01-14")
+VALUES ("Tales of Baneheath - Red", "Comics & Illustration", 25000, "2017-12-01", "2018-01-14");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("MIRAGE: A Feature-Packed Delay Pedal by RedShift Effects", "Design & Tech", 9000, "2017-11-25", "2018-01-17")
+VALUES ("MIRAGE: A Feature-Packed Delay Pedal by RedShift Effects", "Design & Tech", 9000, "2017-11-25", "2018-01-17");
